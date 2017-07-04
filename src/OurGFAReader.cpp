af10c29553c5283b02b2e5e317cc19d872f8c8d3
Fatemeh <sfmodarres@gmail.com>
2017-07-04 11:02:35 -0400

    cleansing OurGFAReader

--- src/OurGFAReader.cpp
+++ src/OurGFAReader.cpp
@@ -4,63 +4,7 @@
 #include "xxhash.h"
 #include <algorithm>
 #include <string>
-
-std::vector<std::pair<uint64_t, bool>>
-PosFinder::explode(const stx::string_view str, const char& ch) {
-  std::string next;
-  std::vector<std::pair<uint64_t, bool>> result;
-  // For each character in the string
-  for (auto it = str.begin(); it != str.end(); it++) {
-    // If we've hit the terminal character
-    if (*it == '+' or *it == '-') {
-      bool orientation = true;
-      // If we have some characters accumulated
-      // Add them to the result vector
-      if (!next.empty()) {
-        if (*it == '-') {
-          orientation = false;
-        }
-        result.emplace_back(std::stoll(next), orientation);
-        next.clear();
-      }
-    } else if (*it != ch) {
-      // Accumulate the next character into the sequence
-      next += *it;
-    }
-  }
-  if (!next.empty())
-    result.emplace_back(std::stoll(next),
-                        true); // this case shouldn't even happen
-  return result;
-}
-
-bool PosFinder::is_number(const std::string& s) {
-  return !s.empty() && std::find_if(s.begin(), s.end(), [](char c) {
-                         return !std::isdigit(c);
-                       }) == s.end();
-}
-
-// Avoiding un-necessary stream creation + replacing strings with string view
-// is a bit > than a 2x win!
-// implementation from : https://marcoarena.wordpress.com/tag/string_view/
-std::vector<stx::string_view> PosFinder::split(stx::string_view str,
-                                               char delims) {
-  std::vector<stx::string_view> ret;
-
-  stx::string_view::size_type start = 0;
-  auto pos = str.find_first_of(delims, start);
-  while (pos != stx::string_view::npos) {
-    if (pos != start) {
-      ret.push_back(str.substr(start, pos - start));
-    }
-    start = pos + 1;
-    pos = str.find_first_of(delims, start);
-  }
-  if (start < str.length()) {
-    ret.push_back(str.substr(start, str.length() - start));
-  }
-  return ret;
-}
+#include "Util.hpp"
 
 PosFinder::PosFinder(const char* gfaFileName, size_t input_k) {
   filename_ = std::string(gfaFileName);
@@ -68,30 +12,6 @@ PosFinder::PosFinder(const char* gfaFileName, size_t input_k) {
   file.reset(new zstr::ifstream(gfaFileName));
   k = input_k;
 }
-
-/*
-void PosFinder::scanContigLengths() {
-  std::string ln;
-  std::string tag, id, value;
-  size_t contig_cnt{0};
-  size_t contig_len{0};
-  size_t ref_cnt{0};
-  while(std::getline(*file, ln)) {
-    char firstC = ln[0];
-    if (firstC != 'S') continue;
-    stx::string_view lnview(ln);
-    std::vector<stx::string_view> splited = split(lnview, '\t');
-    if (is_number(id)) {
-      contig_len += splited[1].length();
-      tag = splited[0].to_string();
-      id = splited[1].to_string();
-      value = splited[2].to_string();
-      contigid2seq[id] = value;
-      contig_cnt++;
-    }
-  }
-}
-*/
 
 size_t PosFinder::fillContigInfoMap_() {
   std::string ln;
@@ -103,7 +23,7 @@ size_t PosFinder::fillContigInfoMap_() {
     if (firstC != 'S')
       continue;
     stx::string_view lnview(ln);
-    std::vector<stx::string_view> splited = split(lnview, '\t');
+    std::vector<stx::string_view> splited = util::split(lnview, '\t');
     try {
       auto nid = std::stoull(splited[1].to_string());
       (void)nid;
@@ -150,7 +70,7 @@ void PosFinder::parseFile() {
     if (firstC != 'S' and firstC != 'P')
       continue;
     stx::string_view lnview(ln);
-    std::vector<stx::string_view> splited = split(lnview, '\t');
+    std::vector<stx::string_view> splited = util::split(lnview, '\t');
     tag = splited[0].to_string();
     id = splited[1].to_string();
     // value = splited[2].to_string();
@@ -168,14 +88,12 @@ void PosFinder::parseFile() {
     // A path line
     if (tag == "P") {
       auto pvalue = splited[2];
-      std::vector<std::pair<uint64_t, bool>> contigVec = explode(pvalue, ',');
+      std::vector<std::pair<uint64_t, bool>> contigVec = util::explode(pvalue, ',');
       // parse value and add all conitgs to contigVec
 
       path[ref_cnt] = contigVec;
       refMap.push_back(id);
       ref_cnt++;
-      // refMap[ref_cnt] = id;
-      // refIDs[id] = ref_cnt++;
     }
   }
   std::cerr << " Total # of Contigs : " << contig_cnt
@@ -188,51 +106,8 @@ spp::sparse_hash_map<uint64_t, util::PackedContigInfo>&
 PosFinder::getContigNameMap() {
   return contigid2seq;
 }
-spp::sparse_hash_map<std::string, std::string>& PosFinder::getContigIDMap() {
-  return seq2contigid;
-}
 
-/*
-spp::sparse_hash_map<uint32_t, std::string>& PosFinder::getRefIDs() {
-  return refMap;
-}
-*/
 std::vector<std::string>& PosFinder::getRefIDs() { return refMap; }
-
-std::map<std::pair<std::string, bool>, bool, util::cmpByPair>&
-PosFinder::getPathStart() {
-  return pathStart;
-}
-std::map<std::pair<std::string, bool>, bool, util::cmpByPair>&
-PosFinder::getPathEnd() {
-  return pathEnd;
-}
-
-std::vector<std::pair<std::string, std::string>>& PosFinder::getNewSegments() {
-  return newSegments;
-}
-
-pufg::Graph& PosFinder::getSemiCG() { return semiCG; }
-
-/*
-void PosFinder::writeFile(std::string fileName){
-    std::ofstream gfa_file(fileName) ;
-    for(auto& cseq : contigid2seq){
-        gfa_file << "S" << "\t" << cseq.first <<"\t" << cseq.second << "\n" ;
-    }
-    for(auto& p : path){
-        auto tid = p.first ;
-        gfa_file << "P" << "\t" << tid << "\t"  ;
-        auto vec = p.second ;
-        for(size_t i = 0 ; i < vec.size()-1 ; i++){
-            gfa_file << vec[i].first << ((vec[i].second)?"+":"-") << "," ;
-        }
-        gfa_file << vec[vec.size()-1].first <<
-((vec[vec.size()-1].second)?"+":"-") << "\t*\n";
-
-    }
-}
-*/
 
 void PosFinder::mapContig2Pos() {
   uint64_t pos = 0;
@@ -294,18 +169,6 @@ void PosFinder::serializeContigTable(const std::string& odir) {
       }
     };
 
-    // Write out contig offsets and lengths
-    /*
-    {
-      std::vector<util::ContigPosInfo> cpi;
-      cpi.reserve(contigid2seq.size());
-      for (auto& kv : contigid2seq) {
-        cpi.push_back({kv.second.offset, kv.second.length});
-      }
-      ar(cpi);
-    }
-    */
-
     spp::sparse_hash_map<std::vector<uint32_t>, uint32_t, VecHasher> eqMap;
     std::vector<uint32_t> eqIDs;
     std::vector<std::vector<util::Position>> cpos;
@@ -325,16 +188,6 @@ void PosFinder::serializeContigTable(const std::string& odir) {
         eqMap[tlist] = eqID;
       }
       eqIDs.push_back(eqID);
-      // ar(contig2pos[kv.first]);
-      /*
-      auto& ent = contig2pos[kv.first];
-      ct << kv.first;
-      for (auto const & ent2 : ent) {
-        ct << '\t' << ent2.transcript_id << ',' << ent2.pos << ',' <<
-      ent2.orien;
-      }
-      ct << '\n';
-      */
     }
     std::cerr << "there were " << eqMap.size() << " equivalence classes\n";
     eqAr(eqIDs);
@@ -353,44 +206,6 @@ void PosFinder::serializeContigTable(const std::string& odir) {
     eqAr(eqLabels);
     ar(cpos);
   }
-  /*
-    ct << refIDs.size() << '\n';
-    for (auto const & ent : refIDs) {
-        ct << ent.first << '\t' << ent.second << '\n';
-    }
-    for (auto const & ent : contig2pos) {
-        ct << ent.first;
-        for (auto const & ent2 : ent.second) {
-            ct << '\t' << ent2.transcript_id << ',' << ent2.pos << ',' <<
-    ent2.orien;
-        }
-        ct << '\n';
-    }
-  */
 }
 
-void PosFinder::deserializeContigTable() {
-  // TODO read the file in the same order as you've written it down.
-}
-/*
-int main(int argc, char* argv[]) {
-    //std::vector<std::string> gfa_file = {argv[1]};
-    //std::vector<std::string> read_file = {argv[2]};
-    std::cerr<<" Starting ... \n";
-    PosFinder pf(argv[1], 30);
-    pf.parseFile();
 
-    pf.mapContig2Pos();
-
-    std::ofstream ct("contig_table.tsv");
-    for (auto const & ent : pf.contig2pos) {
-        ct << ent.first;
-        for (auto const & ent2 : ent.second) {
-            ct << '\t' << ent2.transcript_id << ',' << ent2.pos << ',' <<
-ent2.orien;
-        }
-        ct << '\n';
-    }
-    ct.close();
-}
-*/
